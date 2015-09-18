be9f86180ca5828aeb2bc09c6a4d64d85f7e0329
demidrolll <demi-drolll@yandex.ru>
2015-09-18 23:23:24 +0300

    черновая структура бд

---
+++ Terminal/db/DutyTable.sql
@@ -0,0 +1,6 @@
+create table duties (
+  id_duty int auto_increment primary key,
+  id_term int,
+  time_open datetime,
+  time_close datetime
+);
---
+++ Terminal/db/SprOperationTable.sql
@@ -0,0 +1,4 @@
+create table operation_spr (
+  id_type int primary key auto_increment,
+  name varchar(60)
+);
