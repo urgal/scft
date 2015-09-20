a014321000ef5b6769e3451cf2bc802a19180441
Dzmitry Brukhanchyk <bdm@tut.by>
2015-09-20 08:23:14 +0200

    Юниты к приему/отправке сообщений - терминал

---
+++ Terminal/xmlrequest.pas
@@ -0,0 +1,38 @@
+unit xmlrequest;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, Grids, DBGrids, StdCtrls, DB, terminal_tools, Data.Win.ADODB,
+  terminal_db, IPPeerClient, REST.Client, REST.Types, Data.Bind.Components,
+  Data.Bind.ObjectScope;
+
+function CreateXMLFile(aTermID, aShiftID, aCurr, aOperType, aPan, aPinBlock, aAmount : string): string;
+
+implementation
+
+uses
+  request100;
+
+function CreateXMLFile(aTermID, aShiftID, aCurr, aOperType, aPan, aPinBlock, aAmount : string): string;
+var
+  vRequest : IXMLRequestType100;
+  vXML : string;
+begin
+  vRequest := request100.Newrequest;
+
+  vRequest.Type_ := '100';
+  vRequest.Poslun := aTermID;
+  vRequest.Batch := strtoint(aShiftID);
+  vRequest.Body.Code := aOperType;
+  vRequest.Body.PAN := aPan;
+  vRequest.Body.Currency := strtoint(aCurr);
+  vRequest.Body.Amount := StrToFloat(aAmount);
+  vRequest.Body.PINblock := aPinBlock;
+  vXML := vRequest.XML;
+  Result := vXML;
+end;
+
+
+end.
---
+++ Terminal/xmlresponse.pas
@@ -0,0 +1,14 @@
+unit xmlresponse;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, Grids, DBGrids, StdCtrls, DB, terminal_tools, Data.Win.ADODB, terminal_db,
+  IPPeerClient, REST.Client, REST.Types,
+  Data.Bind.Components, Data.Bind.ObjectScope;
+
+implementation
+
+
+end.
